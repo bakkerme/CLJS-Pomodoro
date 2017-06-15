98ca0cf7aa04a8037c158320174734ad5e8472a8
Brandon Bakker <brandon@bdmd.com.au>
2017-06-16 08:04:03 +1000

    Working countdown on button, no auto stop yet

--- src/pomodoro/core.cljs
+++ src/pomodoro/core.cljs
@@ -10,9 +10,9 @@
 (def app-state
   (reagent/atom
     {:current-time (time/time-now)
-     :time-at-pom-start (time/plus- (time/time-now) (time/minutes 20))
+     :time-at-pom-end (time/time-now)
      :clock-state js/Date.
-     :isRunning true
+     :isRunning false
      :isResting false
      :iterationsFinished 0
     }))
@@ -21,18 +21,8 @@
 (defn format-time [t]
   (format/unparse time-formatter t))
 
-(defn tick []
-  (swap! app-state assoc :current-time (time/time-now)))
-
 (defn get-clock-running-state []
   (get @app-state :isRunning))
-
-;; -------------------------
-;; Views
-
-(defn clock-running
-  [clockStatus]
-     (str "Clock is " (if clockStatus "running" "not running" )))
 
 (defn clock [time]
   (format-time time))
@@ -41,13 +31,51 @@
   (if (time/after? since current)
     (time/in-seconds (time/interval current since))))
 
+(defn get-time-til-pom-end []
+  (let [current-time (:current-time @app-state)
+        since-time (:time-at-pom-end @app-state)]
+  (get-time-since since-time current-time)))
+
+(defn start-clock []
+  (swap! app-state assoc :time-at-pom-end  (time/plus- (time/time-now) (time/seconds 10)))
+  (swap! app-state assoc :isRunning true))
+
+(defn stop-clock []
+  (swap! app-state assoc :time-at-pom-end  (time/time-now))
+  (swap! app-state assoc :isRunning false))
+
+(defn toggle-clock-status []
+  (if (not (get @app-state :isRunning)) (start-clock) (stop-clock) ))
+
+(defn tick []
+  (swap! app-state assoc :current-time (time/time-now))
+  (if (get @app-state :isRunning) 
+    (if (= (get-time-til-pom-end) 0)
+      (stop-clock))))
+
+(defn start-tick []
+  (.setInterval js/window tick 1000))
+
+;; -------------------------
+;; Views
+
+(defn toggle-clock-wrapper [onClick]
+  [:button {:on-click (fn [] (onClick))}
+   "Toggle clock state"])
+
+
+(defn clock-running
+  [clockStatus]
+     (str "Clock is " (if clockStatus "running" "not running" )))
+
 (defn app-wrapper []
   (let [current-time (:current-time @app-state)
-        since-time (:time-at-pom-start @app-state)]
+        since-time (:time-at-pom-end @app-state)]
     [:div
      [:h1 (clock-running (get-clock-running-state))]
      [:h2 (clock current-time)]
-     [:h2 (get-time-since since-time current-time)]]))
+     [:h2 (if(get @app-state :isRunning) (get-time-since since-time current-time))]
+     [:div (toggle-clock-wrapper toggle-clock-status)]]))
 
 ;; -------------------------
 ;; Initialize app
@@ -55,7 +83,7 @@
 (defn mount-root []
   (reagent/render [app-wrapper] (.getElementById js/document "app")))
 
-(.setInterval js/window tick 1000)
 
 (defn init! []
+  (start-tick)
   (mount-root))
